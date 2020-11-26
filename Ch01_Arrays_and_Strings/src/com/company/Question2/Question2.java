355beeb249d88dec5b9a347e3fc5dcdbc4db84a4
Kavin Kumar Arumuagm <42685150+kaphc@users.noreply.github.com>
2020-11-25 17:03:08 -0800

    Add Question 2,3,4

--- Ch01_Arrays_and_Strings/src/com/company/Question2/Question2.java
+++ Ch01_Arrays_and_Strings/src/com/company/Question2/Question2.java
@@ -1,4 +1,45 @@
 package com.company.Question2;
 
 public class Question2 {
+
+    public static boolean isPermutation(String firstWord, String secondWord) {
+
+        /* Ask interviewer about whether it is
+        1. Unicode or ASCII
+        2. Will it have extra space in between or at the ends
+        3. Should the comparison be case sensitive
+        */
+
+        firstWord = firstWord.trim();
+        secondWord = secondWord.trim();
+
+        int[] charSet = new int[128];
+
+        if(firstWord.length() != secondWord.length()) return false;
+
+        for(int i=0; i<firstWord.length(); i++) {
+            charSet[firstWord.charAt(i)]++;
+        }
+
+        int charInInt;
+
+        for(int j=0; j<secondWord.length(); j++) {
+            charInInt = secondWord.charAt(j);
+            charSet[charInInt]--;
+            if(charSet[charInInt] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPermutation("kavin", "kavin     "));
+        System.out.println(isPermutation("kavin", "   "));
+        System.out.println(isPermutation("", "kavin"));
+        System.out.println(isPermutation("kavin", "kumar     "));
+        System.out.println(isPermutation("kavin", "vkani"));
+        System.out.println(isPermutation("kavin", "   vkani"));
+        System.out.println(isPermutation("kavin", "vka ni"));
+    }
 }
---
+++ Ch01_Arrays_and_Strings/src/com/company/Question4/Question4.java
@@ -0,0 +1,37 @@
+package com.company.Question4;
+
+public class Question4 {
+
+    public static boolean isPallindromePermutation(String word) {
+
+        // Ask interviewer whether it is Unicode or ASCII
+        int[] charSet = new int[128];
+        int oddCountCharacters = 0;
+        int charInInt;
+
+        for(int i=0; i<word.length(); i++) {
+            charInInt = word.charAt(i);
+
+            charSet[charInInt]++;
+            if(charSet[charInInt]%2 == 0) {
+                oddCountCharacters--;
+            } else {
+                oddCountCharacters++;
+            }
+        }
+
+        return oddCountCharacters<=1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPallindromePermutation("nkaiavkinv"));
+        System.out.println(isPallindromePermutation("tact coa"));
+        System.out.println(isPallindromePermutation("kavin"));
+        System.out.println(isPallindromePermutation("kavin     nivak"));
+        System.out.println(isPallindromePermutation(" "));
+        System.out.println(isPallindromePermutation(""));
+        System.out.println(isPallindromePermutation("ka vi n nak iv"));
+        System.out.println(isPallindromePermutation("ka xvi n nak iv"));
+        System.out.println(isPallindromePermutation("ka xvi n snak iv"));
+    }
+}
