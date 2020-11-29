76e2a8be6eac1a6c0c66a34e30a9dec0a38240a8
Kavin Kumar Arumuagm <42685150+kaphc@users.noreply.github.com>
2020-11-28 19:48:15 -0800

    Fix Question4 and DataStructure

--- Ch02_Linked_Lists/src/com/company/Question4/Question4.java
+++ Ch02_Linked_Lists/src/com/company/Question4/Question4.java
@@ -11,43 +11,38 @@ public class Question4 {
         DoublyLinkedListNode<Integer> nextNode;
         DoublyLinkedListNode<Integer> tail;
 
-        while (currentNode.getNext() != initialTail) {
+        while (currentNode != initialTail) {
             nextNode = currentNode.getNext();
-
             if (currentNode.getValue() > partition) {
-                if (currentNode.getPrevious() != null) {
+                if (currentNode == list.getHead()) {
+                    list.removeHead();
+                } else {
                     currentNode.getPrevious().setNext(currentNode.getNext());
                     currentNode.getNext().setPrevious(currentNode.getPrevious());
-                } else {
-                    currentNode.getNext().setPrevious(null);
                 }
-
-                tail = list.getTail();
-                currentNode.setPrevious(tail);
-                tail.setNext(currentNode);
-                currentNode.setNext(null);
+                list.addTail(currentNode);
             }
             currentNode = nextNode;
         }
     }
 
     public static void main(String[] args) {
-//        Integer[] elements1 = new Integer[]{3, 5, 8, 5, 10, 2, 1};
-//        DoublyLinkedList<Integer> list1 = new DoublyLinkedList<>(elements1);
-//        list1.displayLinkedList();
-//        partition(list1, 4);
-//        list1.displayLinkedList();
-//
-        Integer[] elements2 = new Integer[]{10, 7, 6, 3, 10, 6, 2};
+        Integer[] elements1 = new Integer[]{8, 5, 8, 5, 10, 2, 1};
+        DoublyLinkedList<Integer> list1 = new DoublyLinkedList<>(elements1);
+        list1.displayLinkedList();
+        partition(list1, 4);
+        list1.displayLinkedList();
+
+        Integer[] elements2 = new Integer[]{2, 7, 6, 3, 10, 6, 10};
         DoublyLinkedList<Integer> list2 = new DoublyLinkedList<>(elements2);
         list2.displayLinkedList();
         partition(list2, 8);
         list2.displayLinkedList();
 
-//        Integer[] elements3 = new Integer[]{3, 2, 3};
-//        DoublyLinkedList<Integer> list3 = new DoublyLinkedList<>(elements3);
-//        list3.displayLinkedList();
-//        partition(list3, 2);
-//        list3.displayLinkedList();
+        Integer[] elements3 = new Integer[]{3, 2, 3};
+        DoublyLinkedList<Integer> list3 = new DoublyLinkedList<>(elements3);
+        list3.displayLinkedList();
+        partition(list3, 2);
+        list3.displayLinkedList();
     }
 }
